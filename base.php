46809abaf0b9fc65e08b398d08e5617155c84125
robin_bailleux <robin.bailleux@gmail.com>
2016-11-12 08:51:21 +0100

    filename, create login.class.php

--- base.php
+++ base.php
@@ -1,6 +1,9 @@
 <?php
-	include dirname(__FILE__).'/core/class/utilit.class.php';
-
+	include dirname(__FILE__).'/core/class/rbDatabase.class.php';
+	include dirname(__FILE__).'/core/class/login.class.php';
+	
 	$install = new rbDatabase();
 	$install->installMainDatabase();
+	
+	session_start();
 ?>
